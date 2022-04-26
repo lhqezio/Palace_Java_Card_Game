703010a21c73fc3b7b8075fb1e9bebaffdd5e2f6
lhqezio <94709107+lhqezio@users.noreply.github.com>
2022-04-26 11:13:03 -0400

    Create UserInput.java

---
+++ src/UserInput.java
@@ -0,0 +1,15 @@
+import Java.util.Scanner
+public class UserInput{
+  final private char[] menuInput = new char[]{'1','2','3','4'};
+  private boolean valid;
+  private scanner sc;
+  UserInput(){
+    this.sc = new Scanner(System.in);
+    this.valid=false;
+    }
+  public char menu(){
+    char input = sc.next();
+    for
+    
+    
+      
