6bddbbc9626e62de230e6eb9492f50000c62589b
lhqezio <94709107+lhqezio@users.noreply.github.com>
2022-04-26 19:43:31 +0000

    update

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,15 +1,29 @@
 import Java.util.Scanner
 public class UserInput{
-  final private char[] menuInput = new char[]{'1','2','3','4'};
-  private boolean valid;
   private scanner sc;
+  private final String welMesg = "Please Input your option";
+  private final String errMesg = "Invalid value,Please Try again";
   UserInput(){
     this.sc = new Scanner(System.in);
-    this.valid=false;
     }
-  public char menu(){
-    char input = sc.next();
-    for
+  public int menu(){
+    System.out.println(welMsg);
+    while(sc<1||sc>4){
+      try{
+        int input = sc.nextInt();
+        if(sc<1||sc>4){
+          throws java.util.InputMismatchException;
+        }
+      }
+      catch (java.util.InputMismatchException e){
+        System.out.println(errMesg);
+        sc.next();
+      }
+    }
+    return input;
+  }
+}
+    
     
     
       
