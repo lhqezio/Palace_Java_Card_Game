b4d84f17a402e32431ccb60c8252de6ea42d6749
Hoang Quoc Le <hoang.le@dawsoncollege.qc.ca>
2022-04-28 13:43:59 +0000

    Update a Table

--- src/Table.java
+++ src/Table.java
@@ -1,3 +1,32 @@
 public class Table {
-    
+    private Deck mainDeck;
+    private Deck playingPile;
+    private Deck[][] playerDeck;
+    Table (int playerNum){
+        if(playerNum<2){
+            throw new IllegalArgumentException("Player number can be negative");
+        }
+        playerDeck = new Deck[playerNum][3];
+        for(int i = 0;i<playerNum;i++){
+            if(i==0){
+                addNewDecktoMain();
+                if(playerNum>2){
+                    addNewDecktoMain();
+                }
+                mainDeck.shuffle();
+            }
+
+        }
+    }
+    private void addNewDecktoMain(){
+        for(int z = 2;z<=13;z++){
+            mainDeck.put(new Card(Card.suit.SPADE,z));
+            mainDeck.put(new Card(Card.suit.HEART,z));
+            mainDeck.put(new Card(Card.suit.CLUB,z));
+            mainDeck.put(new Card(Card.suit.CLUB,z));
+        }
+    }
+    public boolean draw(int player) {
+
+    }
 }
